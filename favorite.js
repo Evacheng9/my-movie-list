76fd4db24501787dc082c8c40488ae1be43256d4
Eva Cheng <spinco1001@gmail.com>
2020-04-30 00:08:50 +0800

    Update movie list
    - add ganre hashtag of movies
    - change the way to show movie details

--- favorite.js
+++ favorite.js
@@ -1,52 +1,120 @@
 (function () {
-  const BASE_URL = 'https://movie-list.alphacamp.io'
-  const INDEX_URL = BASE_URL + '/api/v1/movies/'
-  const POSTER_URL = BASE_URL + '/posters/'
-  const dataPanel = document.getElementById('data-panel')
+
+  const URL = 'https://movie-list.alphacamp.io'
+  const index_URL = URL + '/api/v1/movies/'
+  const poster_URL = URL + '/posters/'
   const data = JSON.parse(localStorage.getItem('favoriteMovies')) || []
+  const dataPenal = document.getElementById('data-panel')
+  const searchForm = document.getElementById('search')
+  const searchInput = document.getElementById('search-input')
 
-  displayDataList(data)
 
-  function displayDataList(data) {
-    let htmlContent = ''
-    data.forEach(function (item, index) {
-      htmlContent += `
-        <div class="col-sm-3">
-          <div class="card mb-2">
-            <img class="card-img-top " src="${POSTER_URL}${item.image}" alt="Card image cap">
-            <div class="card-body movie-item-body">
-              <h5 class="card-title">${item.title}</h5>
-            </div>
-            <div class="card-footer">
-              <button class="btn btn-primary btn-show-movie" data-toggle="modal" data-target="#show-movie-modal" data-id="${item.id}">More</button>
-              <button class="btn btn-danger btn-remove-favorite" data-id="${item.id}">X</button>
-            </div>
-          </div>
-        </div>
-      `
-    })
-    dataPanel.innerHTML = htmlContent
-  }
+  displayMovies(data)
 
-  dataPanel.addEventListener('click', (event) => {
+
+  dataPenal.addEventListener('click', (event) => {
     if (event.target.matches('.btn-show-movie')) {
       showMovie(event.target.dataset.id)
-    } else if (event.target.matches('.btn-remove-favorite')) {
+    } else if (event.target.matches('.btn-delete')) {
       removeFavoriteItem(event.target.dataset.id)
     }
   })
 
+  dataPenal.addEventListener('mouseover', (event) => {
+    if (event.target.matches('.btn-show-movie')) {
+      event.target.style = " cursor: pointer;"
+    }
+  })
+
+  function displayMovies(data) {
+    let htmlContent = ''
+    data.forEach(item => {
+      htmlContent += `
+      <div class="col-sm-3">
+        <div class="card m-2">
+          <h6 class="card-header">${item.title}</h6>
+          <div class="card-body">
+            <div data-toggle="modal" data-target="#show-movie-modal">
+              <img class="btn-show-movie card-img-top" src="${poster_URL}${item.image}" data-id="${item.id}">
+            </div>
+            <div id="genres">
+              <p class="card-text">${transformGenres(item.genres)}</p>
+            </div>
+            <!-- delete button -->
+            <div class="row justify-content-center mt-3">
+              <button class="btn btn-danger btn-delete" data-id="${item.id}">Delete</button>
+            </div>       
+          </div>
+        </div>
+      </div>`
+    })
+    dataPenal.innerHTML = htmlContent
+    console.log(data)
+  }
+
+  function transformGenres(genresNumArray) {
+    const genresType = {
+      "1": "Action",
+      "2": "Adventure",
+      "3": "Animation",
+      "4": "Comedy",
+      "5": "Crime",
+      "6": "Documentary",
+      "7": "Drama",
+      "8": "Family",
+      "9": "Fantasy",
+      "10": "History",
+      "11": "Horror",
+      "12": "Music",
+      "13": "Mystery",
+      "14": "Romance",
+      "15": "Science Fiction",
+      "16": "TV Movie",
+      "17": "Thriller",
+      "18": "War",
+      "19": "Western"
+    }
+    let genresArray = []
+    let genreHashtag = ''
+    genresNumArray.forEach(e => {
+      if (e.toString() === Object.keys(genresType)[e - 1]) {
+        genresArray.push(genresType[e])
+      }
+    })
+    genresArray.forEach(genre => {
+      genreHashtag += `<a href="#" style="white-space:nowrap;">#${genre}</a>&nbsp;`
+    })
+    return genreHashtag
+  }
+
+  function showMovie(id) {
+    // get elements
+    const modalTitle = document.getElementById('show-movie-title')
+    const modalImage = document.getElementById('show-movie-image')
+    const modalDate = document.getElementById('show-movie-date')
+    const modalDescription = document.getElementById('show-movie-description')
+    const url = index_URL + id
+    // send request to show api
+    axios.get(url).then(response => {
+      const data = response.data.results
+      // insert data into modal ui
+      modalTitle.textContent = data.title
+      modalImage.innerHTML = `<img src="${poster_URL}${data.image}" class="img-fluid" alt="Responsive image">`
+      modalDate.textContent = `release at : ${data.release_date}`
+      modalDescription.textContent = `${data.description}`
+    })
+  }
+
   function removeFavoriteItem(id) {
-    // find movie by id
     const index = data.findIndex(item => item.id === Number(id))
     if (index === -1) return
-    //return表示這個函式執行到這邊為止, 後面雖然還有 code, 但是不繼續執行
-    // remove movie and update localStorage
     data.splice(index, 1)
     localStorage.setItem('favoriteMovies', JSON.stringify(data))
-    // repaint dataList
-    displayDataList(data)
+    displayMovies(data)
   }
+
+
+
 
 
 
--- index.js
+++ index.js
@@ -1,27 +1,26 @@
 (function () {
-  const BASE_URL = 'https://movie-list.alphacamp.io'
-  const INDEX_URL = BASE_URL + '/api/v1/movies'
-  const POSTER_URL = BASE_URL + '/posters/'
+  const URL = 'https://movie-list.alphacamp.io'
+  const index_URL = URL + '/api/v1/movies/'
+  const poster_URL = URL + '/posters/'
   const data = []
   const dataPanel = document.getElementById('data-panel')
   const searchForm = document.getElementById('search')
   const searchInput = document.getElementById('search-input')
   const pagination = document.getElementById('pagination')
-  const ITEM_PER_PAGE = 12
+  const itemPerPage = 12
   let paginationData = []
+  const modeChange = document.getElementById('mode')
+  let mode = true
+  let pageNum = 1
 
-  //get API data
-  axios.get(INDEX_URL)
-    .then((response) => {
-      data.push(...response.data.results)
-      displayDataList(data)
-      getTotalPages(data)
+  axios.get(index_URL).then((response) => {
+    response.data.results.forEach(item => {
+      data.push(item)
     })
-    .catch((err) => {
-      console.log('Error!')
-    })
+    getTotalPages(data)
+    getPageData(pageNum, data)
+  }).catch((err) => { console.log(err) })
 
-  // listen to data panel: more & add to favorite list
   dataPanel.addEventListener('click', (event) => {
     if (event.target.matches('.btn-show-movie')) {
       showMovie(event.target.dataset.id)
@@ -30,59 +29,172 @@
     }
   })
 
-  //listen to search bar
+  dataPanel.addEventListener('mouseover', (event) => {
+    if (event.target.matches('.btn-show-movie')) {
+      event.target.style = " cursor: pointer;"
+    }
+  })
+
   searchForm.addEventListener('submit', event => {
     event.preventDefault()
-    let results = []
-    const regex = new RegExp(searchInput.value, 'i')
-    results = data.filter(movie => movie.title.match(regex))
-    console.log(results)
-    // displayDataList(results)
+    let input = searchInput.value.toLowerCase()
+    let results = data.filter(movie => movie.title.toLowerCase().includes(input))
     getTotalPages(results)
-    getPageData(1, results)
+    getPageData(pageNum, results)
+    searchInput.value = ""
   })
 
-  // listen to pagination click event
-  pagination.addEventListener('click', event => {
-    console.log(event.target.dataset.page)
-    if (event.target.tagName === 'A') {
-      getPageData(event.target.dataset.page)
+  pagination.addEventListener('click', e => {
+    // console.log(e.target.dataset.page)
+    if (e.target.tagName === 'A') {
+      getPageData(e.target.dataset.page)
+    }
+    pageNum = event.target.dataset.page
+  })
+
+  modeChange.addEventListener('click', event => {
+    // console.log(pageNum)
+    if (event.target.matches('.img-mode')) {
+      mode = true
+      getTotalPages(data)
+      getPageData(pageNum, data)
+    } else if (event.target.matches('.list-mode')) {
+      mode = false
+      getTotalPages(data)
+      getPageData(pageNum, data)
     }
   })
 
-  // listen to pagination click event
-  pagination.addEventListener('click', event => {
-    console.log(event.target.dataset.page)
-    if (event.target.tagName === 'A') {
-      getPageData(event.target.dataset.page)
+
+  function displayMovies(data) {
+    let htmlContent = ''
+    data.forEach(item => {
+      htmlContent += `
+      <div class="col-sm-3">
+        <div class="card m-2">
+          <h6 class="card-header">${item.title}</h6>
+          <div class="card-body">
+            <div data-toggle="modal" data-target="#show-movie-modal">
+              <img class="btn-show-movie card-img-top" src="${poster_URL}${item.image}" data-id="${item.id}">
+            </div>
+            <div id="genres">
+              <p class="card-text">${transformGenres(item.genres)}</p>
+            </div>       
+            <!-- favorite button --> 
+            <div class="row justify-content-center mt-3">
+              <button class="btn btn-primary btn-add-favorite" data-id="${item.id}">+</button>
+            </div>
+          </div>
+        </div>
+      </div>`
+    })
+    dataPanel.innerHTML = htmlContent
+  }
+
+  function showMovie(id) {
+    // get elements
+    const modalTitle = document.getElementById('show-movie-title')
+    const modalImage = document.getElementById('show-movie-image')
+    const modalDate = document.getElementById('show-movie-date')
+    const modalDescription = document.getElementById('show-movie-description')
+    const url = index_URL + id
+    // send request to show api
+    axios.get(url).then(response => {
+      const data = response.data.results
+      // insert data into modal ui
+      modalTitle.textContent = data.title
+      modalImage.innerHTML = `<img src="${poster_URL}${data.image}" class="img-fluid" alt="Responsive image">`
+      modalDate.textContent = `release at : ${data.release_date}`
+      modalDescription.textContent = `${data.description}`
+    })
+  }
+
+  function transformGenres(genresNumArray) {
+    const genresType = {
+      "1": "Action",
+      "2": "Adventure",
+      "3": "Animation",
+      "4": "Comedy",
+      "5": "Crime",
+      "6": "Documentary",
+      "7": "Drama",
+      "8": "Family",
+      "9": "Fantasy",
+      "10": "History",
+      "11": "Horror",
+      "12": "Music",
+      "13": "Mystery",
+      "14": "Romance",
+      "15": "Science Fiction",
+      "16": "TV Movie",
+      "17": "Thriller",
+      "18": "War",
+      "19": "Western"
     }
-  })
+    let genresArray = []
+    let genreHashtag = ''
+    genresNumArray.forEach(e => {
+      if (e.toString() === Object.keys(genresType)[e - 1]) {
+        genresArray.push(genresType[e])
+      }
+    })
+    genresArray.forEach(genre => {
+      genreHashtag += `<a href="#" style="white-space:nowrap;">#${genre}</a>&nbsp;`
+    })
+    return genreHashtag
+  }
 
-  axios.get(INDEX_URL).then((response) => {
-    data.push(...response.data.results)
-    getTotalPages(data)
-    getPageData(1, data)
-  }).catch((err) => console.log(err))
+  function addFavoriteItem(id) {
+    const list = JSON.parse(localStorage.getItem('favoriteMovies')) || []
+    const movie = data.find(item => item.id === Number(id))
+    if (list.some(item => item.id === Number(id))) {
+      alert(`${movie.title} is already in the favorite list!`)
+    } else {
+      list.push(movie)
+      console.log(movie)
+      alert(`${movie.title} was added!`)
+      localStorage.setItem('favoriteMovies', JSON.stringify(list))
+    }
+  }
 
+  function getTotalPages(data) {
+    let totalPages = Math.ceil(data.length / itemPerPage) || 1
+    let pageContent = ''
+    for (let i = 0; i < totalPages; i++) {
+      pageContent += `
+        <li class="page-item">
+          <a class="page-link" href="javascript:;" data-page="${i + 1}">${i + 1}</a>
+        </li>
+      `
+    }
+    pagination.innerHTML = pageContent
+  }
 
+  function getPageData(pageNum, data) {
+    paginationData = data || paginationData
+    let offset = (pageNum - 1) * itemPerPage
+    let pageData = paginationData.slice(offset, offset + itemPerPage)
+    //判斷分頁模式
+    if (mode) {
+      displayMovies(pageData)
+    } else if (!mode) {
+      showListMode(pageData)
+    }
+  }
 
-
-  /*-----------------Function here------------------*/
-  function displayDataList(data) {
+  //show list mode
+  function showListMode(data) {
     let htmlContent = ''
     data.forEach(function (item, index) {
       htmlContent += `
-        <div class="col-sm-3">
-          <div class="card mb-2">
-            <img class="card-img-top" src="${POSTER_URL}${item.image}" alt="Card image cap">
-            <div class="card-body movie-item-body">
-              <h6 class="card-title">${item.title}</h6>       
-            </div>  
-            <div class="card-footer">
-              <!-- "More" button -->
+        <div class="row col-12 justify-content-between m-2" id="list">
+          <div>${item.title}</div>
+          <div class="row justify-content-around">
+            <div class="ml-2 mr-2">    
               <button class="btn btn-primary btn-show-movie" data-toggle="modal" data-target="#show-movie-modal" data-id="${item.id}">More</button>
-              <!-- favorite button -->
-              <button class="btn btn-info btn-add-favorite" data-id="${item.id}">++</button>
+            </div>
+            <div>  
+              <button class="btn btn-info btn-add-favorite" data-id="${item.id}">+</button>
             </div>
           </div>
         </div>`
@@ -90,57 +202,6 @@
     dataPanel.innerHTML = htmlContent
   }
 
-  //Show movie detail
-  function showMovie(id) {
-    const modalTitle = document.getElementById('show-movie-title')
-    const modalImage = document.getElementById('show-movie-image')
-    const modalDate = document.getElementById('show-movie-date')
-    const modalDescription = document.getElementById('show-movie-description')
-    const url = INDEX_URL + '/' + id
-    console.log(url)
-    axios.get(url).then(response => {
-      const data = response.data.results
-      console.log(data)
-      modalTitle.textContent = data.title
-      modalImage.innerHTML = `<img src="${POSTER_URL}${data.image}" class="img-fluid" alt="Responsive image">`
-      modalDate.textContent = `release at : ${data.release_date}`
-      modalDescription.textContent = `${data.description}`
-    })
-  }
 
-  //Save favorite list
-  function addFavoriteItem(id) {
-    const list = JSON.parse(localStorage.getItem('favoriteMovies')) || []
-    const movie = data.find(item => item.id === Number(id))
-    if (list.some(item => item.id === Number(id))) {
-      alert(`${movie.title} is already in your favorite list.`)
-    } else {
-      list.push(movie)
-      alert(`Added ${movie.title} to your favorite list!`)
-    }
-    localStorage.setItem('favoriteMovies', JSON.stringify(list))
-  }
-
-  //建立分頁標籤
-  function getTotalPages(data) {
-    let totalPages = Math.ceil(data.length / ITEM_PER_PAGE) || 1
-    let pageItemContent = ''
-    for (let i = 0; i < totalPages; i++) {
-      pageItemContent += `
-        <li class="page-item">
-          <a class="page-link" href="javascript:;" data-page="${i + 1}">${i + 1}</a>
-        </li>
-      `
-    }
-    pagination.innerHTML = pageItemContent
-  }
-
-  //把資料放入分頁標籤中
-  function getPageData(pageNum, data) {
-    paginationData = data || paginationData
-    let offset = (pageNum - 1) * ITEM_PER_PAGE
-    let pageData = paginationData.slice(offset, offset + ITEM_PER_PAGE)
-    displayDataList(pageData)
-  }
 
 })()
